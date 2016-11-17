0a130c54070c97ad0555c0a005a544db52f05715
Milan Vojnovič <vojnovic@avast.com>
2016-11-17 16:50:08 +0100

    attribute encapsulation vi a @property, pep8 style guide

---
+++ Section5/31_attribute_encapsulation.py
@@ -0,0 +1,57 @@
+"""
+Attribute encapsulation via classic getter/setter paradigm
+
+- vary un-pythonic
+"""
+
+
+class MyClass(object):
+    def set_value(self, value):
+        self.value = value
+
+    def get_value(self):
+        return self.value
+
+
+a = MyClass()
+b = MyClass()
+
+a.set_value(10)
+b.set_value(100)
+
+print(a.get_value())
+print(b.get_value())
+
+"""
+better way using @property decorator
+"""
+
+
+class GetSet(object):
+    def __init__(self, value):
+        self.attrval = value
+
+        @property
+        def var(self):
+            print("getting the 'var' attribute")
+            return self.attrval
+
+        @var.setter
+        def var(self, value):
+            print("setting the 'var' attribute")
+            self.attrval = value
+
+        @var.deleter
+        def var(self):
+            print("deleting the 'var' attribute")
+            self.attrval = None
+
+
+me = GetSet(5)
+
+me.var = 1000
+print(me.var)
+# causes AttributeError: 'GetSet' object has no attribute 'var'
+# Python2 vs Python3
+del me.var
+print(me.var)
---
+++ Section5/32_private_variable_names.py
@@ -0,0 +1,26 @@
+"""
+PEP 8 Style Guide
+PEP - Python Enhancement Proposal
+
+e.g.
+
+Module names:   all_lower_case
+Class and Exceptions: CamelCase
+Globals and locals: all_lower_case
+Functions and methods: all_lower_case
+Constants: ALL_CAPS
+
+Variable naming:
+
+"Public" attributes and variables
+- regular_lower_case
+
+"Private" attributes and variables
+- _single_leading_underscore
+
+"Private" attributes and variables that shouldn't be subclassed
+- __double_leading_underscore
+
+"Magic" atributes
+- __double_underscore__
+"""
