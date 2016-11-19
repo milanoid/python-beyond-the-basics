df3311c1db952ce8409cbced35559d51ac223583
Milan Vojnovič <vojnovic@avast.com>
2016-11-19 20:11:25 +0100

    test for exception

--- Section8/pytest/test_myprogram.py
+++ Section8/pytest/test_myprogram.py
@@ -1,5 +1,20 @@
+"""
+run from command line:
+    py.test
+
+"""
+import pytest
+
 from Section8.pytest import myprogram
+
+
+# import myprogram
 
 
 def test_doubleit():
     assert myprogram.doubleit(10) == 20
+
+
+def test_doubleit_type():
+    with pytest.raises(ValueError):
+        assert myprogram.doubleit("astring")
