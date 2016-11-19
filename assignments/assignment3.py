bcbda709cf0e145d4b25f205511b5c3be7bd158c
Milan Vojnovič <vojnovic@avast.com>
2016-11-19 12:18:16 +0100

    assignment3 - hints: use filename as private attibute

--- assignments/assignment3.py
+++ assignments/assignment3.py
@@ -17,18 +17,18 @@ then read and write keys and values as desired" -- that's it.
 
 
 class ConfigDict(dict):
-    config_file = ''
+    _config_file = ''
 
     def __init__(self, config_file):
-        self.config_file = config_file
-        with open(self.config_file) as file:
+        self._config_file = config_file
+        with open(self._config_file) as file:
             for line in file:
                 key, value = line.split('=')
                 dict.__setitem__(self, key, value)
 
     def __setitem__(self, key, value):
         dict.__setitem__(self, key, value)
-        with open(self.config_file, 'w') as file:
+        with open(self._config_file, 'w') as file:
             for key in self.keys():
                 file.write("{key}={value}\n".format(key=key, value=self[key]))
         file.close()
