3cb6d698f383447cc1ede66c5e1d818c24a1fc83
Milan Vojnovič <vojnovic@avast.com>
2016-11-19 18:21:33 +0100

    yaml save class instance

--- Section7/44_yaml.py
+++ Section7/44_yaml.py
@@ -14,3 +14,29 @@ with open('my.yaml') as fh:
     struct = yaml.load(fh)
 
 print(json.dumps(struct, indent=4, separators=(',', ': ')))
+
+"""
+ storing class instance, the class code must available when calling load()
+"""
+
+
+class MyClass(object):
+    classvar = 10
+
+    def __init__(self, val):
+        self.val = val
+
+    def increment(self):
+        self.val += 1
+
+x = MyClass(5)
+x.increment()
+x.increment()
+
+with open('obj.yaml', 'w') as fh:
+    yaml.dump(x, fh)
+
+with open('obj.yaml') as fh:
+    inst = yaml.load(fh)
+
+print(inst.val)
