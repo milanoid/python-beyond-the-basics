7cde67449dbe0de1dc8fc6a20775a6ed58a29f28
Milan Vojnovič <vojnovic@avast.com>
2016-11-19 17:52:23 +0100

    json (de)serialization

---
+++ Section7/43_json.py
@@ -0,0 +1,11 @@
+import json
+
+with open('backup_config.json') as fh:
+    conf = json.load(fh)
+
+print(conf)
+
+conf['newkey'] = 5.00005
+
+with open('backup_config.json', 'w') as fh:
+    json.dump(conf, fh)
