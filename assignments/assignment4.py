961e3b31c4c0c2388f99efb526c7cced36d92f4d
Milan Vojnovič <vojnovic@avast.com>
2016-11-19 15:08:11 +0100

    code format

--- assignments/assignment4.py
+++ assignments/assignment4.py
@@ -1,8 +1,6 @@
-
 class ConfigKeyError(Exception):
-
     def __init__(self, config_dict, key):
-        available_keys =[key for key in config_dict.keys()]
+        available_keys = [key for key in config_dict.keys()]
         self.message = "Key '{key}' not found. " \
                        "Available keys: {available_keys}".format(key=key, available_keys=available_keys)
 
